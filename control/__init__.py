62339836a86264860f19501aa3ebb0ba48a9ffa1
matheuwr8 <matheusswr8@gmail.com>
2026-09-08 16:19:02 -0300

    feat: desenvolvimento do sorteio e da persistencia de tarefas

--- control/__init__.py
+++ control/__init__.py
@@ -3,3 +3,5 @@ from .algoritmos.herancaDePrioridade import heranca_prioridade
 from .motor import simular as simular_motor
 from .politicas import POLITICAS
 from .simular_escalonamento import simular_escalonamento
+from .gerador import sortear_tarefas, rodar_lote, Faixas
+from .persistencia import salvar_cenario, carregar_cenario
